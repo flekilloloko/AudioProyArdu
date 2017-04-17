20dbe1bc96ded2db98242563baaa055af1d22ddb
Mente Y Alma <lamagiadelalocura@hotmail.com>
2017-04-17 14:53:42 -0300

    Código traducido al español

--- fil_iir_q15.cpp
+++ fil_iir_q15.cpp
@@ -13,54 +13,49 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 // A commercial license for MicroModeler DSP can be obtained at http://www.micromodeler.com/launch.jsp
 
-#include "dig_fil_q15.h"
+#include "fil_iir_q15.h"
 
 #include <stdlib.h> // For malloc/free
 #include <string.h> // For memset
 
-q15_t dig_fil_q15_coefficients[12] = 
+q15_t fil_iir_q15_coeficientes[12];
+int fil_iir_q15_grado;
+int fil_iir_q15_posCorr;
+
+
+fil_iir_q15Type *fil_iir_q15_create( string* tipo, float f0, short grado )
 {
-// Scaled for a 16x16:64 Direct form 1 IIR filter with: 
-// Feedback shift = 14
-// Output shift = 14
-// Input  has a maximum value of 1, scaled by 2^15
-// Output has a maximum value of 2.201848663722923, scaled by 2^13
-
-    3546, 0, -7092, 3546, 17180, -4852,// b0 Q12(0.866), 0, b1 Q12(-1.73), b2 Q12(0.866), a1 Q14(1.05), a2 Q14(-0.296)
-    8192, 0, -16384, 8192, 21642, -10367// b0 Q14(0.500), 0, b1 Q14(-1.00), b2 Q14(0.500), a1 Q14(1.32), a2 Q14(-0.633)
-
-};
-
-
-dig_fil_q15Type *dig_fil_q15_create( void )
-{
-	dig_fil_q15Type *result = (dig_fil_q15Type *)malloc( sizeof( dig_fil_q15Type ) );	// Allocate memory for the object
-	dig_fil_q15_init( result );											// Initialize it
+	fil_iir_q15Type *result = (fil_iir_q15Type *)malloc( sizeof( fil_iir_q15Type ) );	// Allocate memory for the object
+	fil_iir_q15_grado = grado;
+	
+	
+	
+	fil_iir_q15_inic( result );											// inicialize it
 	return result;																// Return the result
 }
 
-void dig_fil_q15_destroy( dig_fil_q15Type *pObject )
+void fil_iir_q15_destruir( fil_iir_q15Type *puntObj )
 {
-	free( pObject );
+	free( puntObj );
 }
 
- void dig_fil_q15_init( dig_fil_q15Type * pThis )
+ void fil_iir_q15_inic( fil_iir_q15Type * puntEste )
 {
-	arm_biquad_cascade_df1_init_q15(	&pThis->instance, dig_fil_q15_numStages, dig_fil_q15_coefficients, pThis->state, dig_fil_q15_postShift );
-	dig_fil_q15_reset( pThis );
-
-}
-
- void dig_fil_q15_reset( dig_fil_q15Type * pThis )
-{
-	memset( &pThis->state, 0, sizeof( pThis->state ) ); // Reset state to 0
-	pThis->output = 0;									// Reset output
+	arm_biquad_cascade_df1_inic_q15(	&puntEste->instancia, fil_iir_q15_grado, fil_iir_q15_coeficientes, puntEste->estados, fil_iir_q15_postShift );
+	fil_iir_q15_reiniciar( puntEste );
 
 }
 
- int dig_fil_q15_filterBlock( dig_fil_q15Type * pThis, short * pInput, short * pOutput, unsigned int count )
+ void fil_iir_q15_reiniciar( fil_iir_q15Type * puntEste )
 {
-	arm_biquad_cascade_df1_q15( &pThis->instance, pInput, pOutput, count );
+	memset( &puntEste->estados, 0, sizeof( puntEste->estados ) ); // reiniciar estados to 0
+	puntEste->salida = 0;									// reiniciar salida
+
+}
+
+ int fil_iir_q15_filterBlock( fil_iir_q15Type * puntEste, short * puntEntrada, short * puntSalida, unsigned int count )
+{
+	arm_biquad_cascade_df1_q15( &puntEste->instancia, puntEntrada, puntSalida, count );
 	return count;
 
 }
--- fil_iir_q15.h
+++ fil_iir_q15.h
@@ -13,46 +13,46 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 // A commercial license for MicroModeler DSP can be obtained at http://www.micromodeler.com/launch.jsp
 
-// Begin header file, dig_fil_q15.h
+// Begin header file, fil_iir_q15.h
 
-#ifndef DIG_FIL_Q15_H_ // Include guards
-#define DIG_FIL_Q15_H_
+#ifndef FIL_IIR_Q15_H_ // Include guards
+#define FIL_IIR_Q15_H_
 
 #define ARM_MATH_CM3	// Use ARM Cortex M3
-#define __FPU_PRESENT 1		// Does this device have a floating point unit?
+//#define __FPU_PRESENT 1		// Does this device have a floating point unit?
 #include <arm_math.h>	// Include CMSIS header
 
 // Link with library: libarm_cortexM3_mathL.a (or equivalent)
 // Add CMSIS/Lib/GCC to the library search path
 // Add CMSIS/Include to the include search path
-extern q15_t dig_fil_q15_coefficients[12];
-static const int dig_fil_q15_numStages = 2;
-static const int dig_fil_q15_postShift = 1;
+extern q15_t fil_iir_q15_coeficientes[12];
+extern int fil_iir_q15_grado;
+extern int fil_iir_q15_posCorr;
 
 typedef struct
 {
-	arm_biquad_casd_df1_inst_q15 instance;
-	q15_t state[8];
-	q15_t output;
-} dig_fil_q15Type;
+	arm_biquad_casd_df1_inst_q15 instancia;
+	q15_t estados[8];
+	q15_t salida;
+} fil_iir_q15Type;
 
 
-dig_fil_q15Type *dig_fil_q15_create( void );
-void dig_fil_q15_destroy( dig_fil_q15Type *pObject );
- void dig_fil_q15_init( dig_fil_q15Type * pThis );
- void dig_fil_q15_reset( dig_fil_q15Type * pThis );
-#define dig_fil_q15_writeInput( pThis, input )  \
-	arm_biquad_cascade_df1_q15( &pThis->instance, &input, &pThis->output, 1 );
+fil_iir_q15Type *fil_iir_q15_create( void );
+void fil_iir_q15_destruir( fil_iir_q15Type *puntObj );
+ void fil_iir_q15_inic( fil_iir_q15Type * puntEste );
+ void fil_iir_q15_reiniciar( fil_iir_q15Type * puntEste );
+#define fil_iir_q15_writeInput( puntEste, input )  \
+	arm_biquad_cascade_df1_q15( &puntEste->instancia, &input, &puntEste->salida, 1 );
 
-#define dig_fil_q15_readOutput( pThis )  \
-	pThis->output
+#define fil_iir_q15_readsalida( puntEste )  \
+	puntEste->salida
 
 
- int dig_fil_q15_filterBlock( dig_fil_q15Type * pThis, short * pInput, short * pOutput, unsigned int count );
-#define dig_fil_q15_outputToFloat( output )  \
-	(( (1.0f/8192) * (output) ))    //2 ^ 13
+ int fil_iir_q15_filterBlock( fil_iir_q15Type * puntEste, short * puntEntrada, short * puntSalida, unsigned int count );
+#define fil_iir_q15_salidaToFloat( salida )  \
+	(( (1.0f/8192) * (salida) ))    //2 ^ 13
 
-#define dig_fil_q15_inputFromFloat( input )  \
+#define fil_iir_q15_inputFromFloat( input )  \
 	((short)(32768f * (input)))		//2 ^ 15
 
-#endif // DIG_FIL_Q15_H_
+#endif // fil_iir_q15_H_
